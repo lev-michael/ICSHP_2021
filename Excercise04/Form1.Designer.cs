72a7063b815d85f9f89a4aa665189ff11960b01b
lev-michael <michael.lev@hrsd.cz>
2021-03-09 20:44:21 +0100

    excercise 04

---
+++ Excercise04/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Excercise04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameListBox = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.correctLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.missedLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.accurancyLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.difficultyLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.difficultProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.NewGameButtonAnyLetter = new System.Windows.Forms.ToolStripButton();
+            this.NewGameFirstLetter = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameListBox
+            // 
+            this.gameListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gameListBox.FormattingEnabled = true;
+            this.gameListBox.ItemHeight = 226;
+            this.gameListBox.Location = new System.Drawing.Point(0, 0);
+            this.gameListBox.MultiColumn = true;
+            this.gameListBox.Name = "gameListBox";
+            this.gameListBox.Size = new System.Drawing.Size(1178, 344);
+            this.gameListBox.TabIndex = 0;
+            this.gameListBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.correctLabel,
+            this.missedLabel,
+            this.accurancyLabel,
+            this.difficultyLabel,
+            this.difficultProgressBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 312);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1178, 32);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // correctLabel
+            // 
+            this.correctLabel.Name = "correctLabel";
+            this.correctLabel.Size = new System.Drawing.Size(88, 25);
+            this.correctLabel.Text = "Correct: 0";
+            // 
+            // missedLabel
+            // 
+            this.missedLabel.Name = "missedLabel";
+            this.missedLabel.Size = new System.Drawing.Size(87, 25);
+            this.missedLabel.Text = "Missed: 0";
+            // 
+            // accurancyLabel
+            // 
+            this.accurancyLabel.Name = "accurancyLabel";
+            this.accurancyLabel.Size = new System.Drawing.Size(116, 25);
+            this.accurancyLabel.Text = "Accuracy: 0%";
+            // 
+            // difficultyLabel
+            // 
+            this.difficultyLabel.Name = "difficultyLabel";
+            this.difficultyLabel.Size = new System.Drawing.Size(77, 25);
+            this.difficultyLabel.Text = "Difficult:";
+            this.difficultyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            // 
+            // difficultProgressBar
+            // 
+            this.difficultProgressBar.Maximum = 800;
+            this.difficultProgressBar.Name = "difficultProgressBar";
+            this.difficultProgressBar.Size = new System.Drawing.Size(200, 24);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewGameButtonAnyLetter,
+            this.NewGameFirstLetter});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1178, 38);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // NewGameButtonAnyLetter
+            // 
+            this.NewGameButtonAnyLetter.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.NewGameButtonAnyLetter.Image = ((System.Drawing.Image)(resources.GetObject("NewGameButtonAnyLetter.Image")));
+            this.NewGameButtonAnyLetter.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.NewGameButtonAnyLetter.Name = "NewGameButtonAnyLetter";
+            this.NewGameButtonAnyLetter.Size = new System.Drawing.Size(250, 33);
+            this.NewGameButtonAnyLetter.Text = "New game - match any letter ";
+            this.NewGameButtonAnyLetter.Click += new System.EventHandler(this.NewGameButtonAnyLetter_Click);
+            // 
+            // NewGameFirstLetter
+            // 
+            this.NewGameFirstLetter.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.NewGameFirstLetter.Image = ((System.Drawing.Image)(resources.GetObject("NewGameFirstLetter.Image")));
+            this.NewGameFirstLetter.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.NewGameFirstLetter.Name = "NewGameFirstLetter";
+            this.NewGameFirstLetter.Size = new System.Drawing.Size(247, 33);
+            this.NewGameFirstLetter.Text = "New game - match first letter";
+            this.NewGameFirstLetter.Click += new System.EventHandler(this.NewGameFirstLetter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 344);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.gameListBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox gameListBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel correctLabel;
+        private System.Windows.Forms.ToolStripStatusLabel missedLabel;
+        private System.Windows.Forms.ToolStripStatusLabel accurancyLabel;
+        private System.Windows.Forms.ToolStripStatusLabel difficultyLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripProgressBar difficultProgressBar;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton NewGameButtonAnyLetter;
+        private System.Windows.Forms.ToolStripButton NewGameFirstLetter;
+    }
+}
+
---
+++ Excercise04/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excercise04
+{
+    enum Mode { AnyLetter, FirstLetter }
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        Stats stats = new Stats();
+        Mode mode;
+
+        public Form1()
+        {
+            InitializeComponent();
+            stats.UpdatedStats += OnUpdateStats;
+            timer1.Stop();
+            timer1.Tick += OnTimerTick;
+        }
+
+        private void OnUpdateStats(object sender, EventArgs e)
+        {
+            correctLabel.Text = $"Correct: {stats.Correct}";
+            missedLabel.Text = $"Missed: {stats.Missed}";
+            accurancyLabel.Text = $"Accuracy: {stats.Accuracy} %";
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            if (gameListBox.Items.Count > 6)
+            {
+                timer1.Stop();
+                gameListBox.KeyDown -= OnKeyDown;
+                gameListBox.Items.Clear();
+                gameListBox.Items.Add("Game over");
+                return;
+            }
+            gameListBox.Items.Add((Keys)random.Next(65, 91));
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (mode == Mode.AnyLetter)
+            {
+                if (gameListBox.Items.Contains(e.KeyCode))
+                {
+                    gameListBox.Items.Remove(e.KeyCode);
+                    gameListBox.Refresh();
+                    updateTimer();
+                    stats.Update(true);
+                }
+                else
+                {
+                    stats.Update(false);
+                }
+            } else
+            {
+                if ((Keys)gameListBox.Items[0] == e.KeyCode)
+                {
+                    gameListBox.Items.RemoveAt(0);
+                    gameListBox.Refresh();
+                    updateTimer();
+                    stats.Update(true);
+                }
+                else
+                {
+                    stats.Update(false);
+                }
+            }
+            int difficulty = 800 - timer1.Interval;
+            difficultProgressBar.Value = difficulty;
+
+        }
+
+        private void updateTimer()
+        {
+            if (timer1.Interval > 400)
+                timer1.Interval -= 60;
+
+            if (timer1.Interval > 250)
+                timer1.Interval -= 15;
+
+            if (timer1.Interval > 150)
+                timer1.Interval -= 8;
+        }
+
+        private void NewGameButtonAnyLetter_Click(object sender, EventArgs e)
+        {
+            mode = Mode.AnyLetter;
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            gameListBox.Items.Clear();
+            stats.clearStats();
+            timer1.Interval = 800;
+            gameListBox.KeyDown += OnKeyDown;
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewGameFirstLetter_Click(object sender, EventArgs e)
+        {
+            mode = Mode.FirstLetter;
+            StartNewGame();
+        }
+    }
+}
